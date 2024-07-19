fec0ced927aeb5b04d6ef320aa16588cd652bde2
Ufrooq <muhammadshafqatali3@gmail.com>
2024-07-19 21:29:01 +0500

    working on welcome page ui

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,22 +1,21 @@
 import { Button } from '@/components/ui/button'
-import React from 'react'
+import mesh_img from "../assets/undraw_landing_page_re_6xev.svg"
 
 const Welcome = () => {
     return (
-        <div className='p-6'>
-            <div>
-                <h3 className='font-base text-xl leading-normal'>
-                    Capture, edit, and share your moments effortlessly with Photogram.
-                </h3>
-                <i className="fa-brands fa-product-hunt text-5xl"></i>
-                <h1 className='font-bold text-5xl'>Welcome to Photogram</h1>
-
+        <div className='flex h-[96vh] items-center justify-around gap-4 p-4'>
+            <div className='p-4 w-[40%] flex flex-col gap-4'>
+                <div className='flex flex-col gap-4'>
+                    <i className="fa-brands fa-product-hunt text-8xl"></i>
+                    <h1 className='font-bold text-5xl'>Welcome to Photogram</h1>
+                    <p className='leading-6 text-justify'>
+                        Join Photogram and experience the joy of capturing and sharing life's beautiful moments. Unleash your creativity with our intuitive tools and vibrant community. Discover a platform designed to inspire and connect photographers of all skill levels. With powerful editing features and seamless sharing options, your photos will always look their best. Start your journey with Photogram today and turn every snapshot into a masterpiece.
+                    </p>
+                    <Button variant="custom" className='self-start px-6 text-md font-semibold'>Explore More</Button>
+                </div>
             </div>
-            <div>
-                <p className='mt-10'>
-                    Join Photogram and experience the joy of capturing and sharing life's beautiful moments. Unleash your creativity with our intuitive tools and vibrant community. Discover a platform designed to inspire and connect photographers of all skill levels. With powerful editing features and seamless sharing options, your photos will always look their best. Start your journey with Photogram today and turn every snapshot into a masterpiece.
-                </p>
-                <Button variant="custom">Explore More</Button>
+            <div className='w-[40%]'>
+                <img src={mesh_img} className="object-fill" alt="" />
             </div>
         </div>
     )
